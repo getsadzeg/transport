20ea1b018af52d5fd35bbd360d6649512f5731fb
getsadzeg <getsadzeguram@gmail.com>
2016-02-22 13:46:53 +0800

    add place classes

---
+++ src/model/Bringplace.java
@@ -0,0 +1,13 @@
+
+package transport.src.model;
+import  transport.src.enums.*;
+
+public class Bringplace {
+    private Address address;
+    public Bringplace() {
+        
+    }
+    public Bringplace(Address address) {
+        this.address = address;
+    }
+}
---
+++ src/model/Destination.java
@@ -0,0 +1,13 @@
+
+package transport.src.model;
+import transport.src.enums.*;
+
+public class Destination {
+    private Address address;
+    public Destination() {
+        
+    }
+    public Destination(Address address) {
+        this.address = address;
+    }
+}
