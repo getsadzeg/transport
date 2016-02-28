c55bc57f411b7dcf66881dbab4b71ddc0df3762e
getsadzeg <getsadzeguram@gmail.com>
2016-02-28 16:17:52 +0400

    encapsulate fields in Route

--- src/model/Route.java
+++ src/model/Route.java
@@ -3,14 +3,30 @@ package transport.src.model;
 import  transport.src.enums.*;
 
 public class Route {
-    private Address bringplace;
+    private Address origin;
     private Address destination;
     public Route() {
         
     }
 
     public Route(Address bringplace, Address destination) {
-        this.bringplace = bringplace;
+        this.origin = bringplace;
+        this.destination = destination;
+    }
+
+    public Address getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Address origin) {
+        this.origin = origin;
+    }
+
+    public Address getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Address destination) {
         this.destination = destination;
     }
     
