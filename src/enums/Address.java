5abeeac19cad1e84ce064d61c527fd44fc9ab7a2
getsadzeg <getsadzeguram@gmail.com>
2016-03-03 14:57:13 +0400

    new routes

--- src/enums/Address.java
+++ src/enums/Address.java
@@ -3,5 +3,7 @@ package transport.src.enums;
 
 
 public enum Address {
-    Beliashvili_St, Abashidze_St, Rustaveli_St, Tamar_Mepe_St, Vaja_Pshavela_St
+    Beliashvili_St, Abashidze_St, Rustaveli_St, Tamar_Mepe_St, Vaja_Pshavela_St,
+    Eristavi_St, Marjanishvili_St, Mirtsxulava_St, Samtredia_St, Freedom_Square,
+    Iosebidze_St
 }
